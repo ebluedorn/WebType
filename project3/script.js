0444cf3c0aff8067e8e1ec52c007c12b41216841
choochoopress <emilyellenbluedorn@gmail.com>
2022-05-05 16:07:48 -0400

    update
    update

--- project3/script.js
+++ project3/script.js
@@ -24,4 +24,33 @@ $(document).ready(function() {
 
 	});
 
+
+	// pop up styling
+	
+
 });
+
+
+jQuery(document).ready(function($){
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
+
+//open popup
+function openpopup(id) {
+    event.preventDefault();
+    	$("#"+id+"").addClass('is-visible');
+}
+
